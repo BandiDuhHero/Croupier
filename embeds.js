d020f20050325efc516d27ef4898f38958a24741
BandiDuhHero <elibandimail@gmail.com>
2021-03-30 19:06:41 -0500

    Update embeds.js

--- embeds.js
+++ embeds.js
@@ -1,40 +1,24 @@
 module.exports = {
     welcome: function(user) {
         return {
-            color: 0x25435d,
-            description: 'Hello ' + user + ' welcome to **{server}**! We hope you enjoy your stay here, we will sure enjoy having you :)',
-            thumbnail: {
-                url: 'https://imgur.com/a/0IHV7J3',
+            color: #8a3fbf,
+            title: 'Welcome!',
+            description: 'Hello ' + user + ' welcome to Solum! We hope you enjoy your stay here, we will surely enjoy having you :)',
+            image: {
+		        url: 'https://i.imgur.com/T1BjY0E.png',
+        	},
+            fields: [
+                {
+                name: 'Verify',
+                value: 'React in rules to gain access to the server',
             },
-            fields: [{
+                {
                 name: 'How to Play',
                 value: 'Use .help to learn how to use Croupier',
             }, ],
         };
     },
-    joinDice: function(p1, p2, winner, loser, amount) {
-        return {
-            color: 0x25435d,
-            description: p2.name + ' joined cuz he aint no bitch\n\n\n' + p1.name + ' rolled a ' + p1.roll +
-                '\n\n' + p2.name + ' rolled a ' + p2.roll + '\n\n' + winner.name + ' has won ' + amount + ' from ' + loser.name,
-            thumbnail: {
-                url: 'http://bestanimations.com/Games/Dice/rolling-dice-gif-3.gif',
-            },
-        };
-    },
-    endDice: function(ender) {
-        return {
-            color: 0x25435d,
-            description: ender + ' has ended the dice he prob was too scared to join:joy:',
-            thumbnail: {
-                url: 'http://bestanimations.com/Games/Dice/rolling-dice-gif-3.gif',
-            },
-            fields: [{
-                name: 'How to Play',
-                value: 'Use .help to learn how to use Croupier',
-            }, ],
-        };
-    },
+
    
     
 };
