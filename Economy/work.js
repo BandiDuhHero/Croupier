34a103edcb137ff1c311fef7449bf1b68f05ffd4
BandiDuhHero <elibandimail@gmail.com>
2021-04-30 03:56:01 -0500

    Update work.js

--- Economy/work.js
+++ Economy/work.js
@@ -3,8 +3,23 @@ class Job {
     this.title = title;
     this.wage = wage;
     this.shift = shift;
-    this.message = message;
+    this.paymsg = message;
   }
 };
 
-exports.programmer = new Job('Programmer', 100, 'daily', 'to-do')
+exports.ceo = new Job('CEO', 1000, 'weekly', '');
+
+exports.rapper = new Job('Rapper', 1500, 'weekly', '');
+
+exports.programmer = new Job('Programmer', 100, 'daily', '');
+
+exports.drugdealer = new Job('Drug Dealer', 70, 'hourly', '');
+
+exports.doctor = new Job('Doctor', 200, 'daily', '');
+
+exports.botanist = new Job('Botanist', 70, 'daily', '');
+
+exports.cook = new Job('Cook', 60, 'daily', '');
+
+exports.cashier = new Job('Cashier', 60, 'daily', '');
+
