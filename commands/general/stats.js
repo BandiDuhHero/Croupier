f288707ac83647e78e329b40147f4cdc9afe5e07
BandiDuhHero <elibandimail@gmail.com>
2021-04-30 05:47:35 -0500

    Create stats.js

---
+++ commands/general/stats.js
@@ -0,0 +1,32 @@
+
+module.exports = {
+ moneyladder: {
+        aliases: ['richestusers', 'richestuser'],
+        execute: function(message, args) {
+            let econ = Economy.economy;
+            let econdata = [];
+            Object.keys(econ).forEach(i => {
+                if (client.users.get(i)) {
+                    econdata.push(econ[i]);
+                }
+            });
+            let compare = function(a, b) {
+                if (a.money < b.money)
+                    return 1;
+                if (a.money > b.money)
+                    return -1;
+                return 0;
+            }
+            econdata.sort(compare);
+            let msg = '';
+            let i = 0;
+            while (i < 11 && econdata[i]) {
+                let place = i + 1;
+                msg += '`(' + place + ') ' + client.users.get(econdata[i].userid).tag + ': ' + econdata[i].money + '💰`\n';
+                i++;
+            }
+            message.channel.send(msg);
+
+        },
+    },
+};
