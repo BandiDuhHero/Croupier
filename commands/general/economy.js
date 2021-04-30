94fb80813096a4995e4fde313240ad47e4d69957
BandiDuhHero <elibandimail@gmail.com>
2021-04-30 05:45:46 -0500

    Update economy.js

--- commands/general/economy.js
+++ commands/general/economy.js
@@ -190,4 +190,30 @@ module.exports = {
 
         },
     },*/
+    work: {
+        execute: async (message, args) => {
+            let job = Economy.economy[message.author.id].job;
+            let chance = Math.floor(Math.random() * 100);
+            let pay = 100;
+            if (!job) {
+                Economy.economy[message.author.id].job = {done: false};
+            }
+            if (job.done) {
+                return await message.channel.send('Your job is already done, come back tommorrow to work again');
+            }
+            if (chance < 10) {
+                return await message.channel.send('There was a accident on the freeway and u missed your shift, come again tommorrow');
+            }
+            
+            else {
+                await Economy.giveMoney(message.author.id, pay);
+                await message.channel.send('You worked and earned ' + pay +  ' ' + Config.currencyName);
+                job.done = true;
+            }
+            if (chance > 75) {
+                await Economy.giveMoney(message.author.id, 80);
+                return await message.channel.send('You got a bonus of 80 ' + Config.currencyName);
+            }
+
+        },
 };
