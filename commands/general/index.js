2c91604fbd045c84c9afa3d2022963ecfa5f13f4
BandiDuhHero <elibandimail@gmail.com>
2021-05-08 14:21:40 -0500

    Update index.js

--- commands/general/index.js
+++ commands/general/index.js
@@ -1,10 +1,11 @@
 
 const dev = require('./dev');
+const stats = require('./stats');
 const help = require('./help');
 const econ = require('./economy');
 const games = require('./games');
 const config = require('./config');
-const commands = Object.assign(econ, help, dev, games, config);
+const commands = Object.assign(econ, help, dev, stats, games, config);
 module.exports = {
     cooldowns: new Discord.Collection(),
 	commands: commands,
