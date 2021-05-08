314569567ba56f2b7ca6c3879782ce7c2715f9f3
BandiDuhHero <elibandimail@gmail.com>
2021-05-08 15:05:28 -0500

    Update stats.js

--- commands/general/stats.js
+++ commands/general/stats.js
@@ -11,11 +11,14 @@ module.exports = {
         execute: function(message, args) {
             let econ = Economy.economy;
             let econdata = [];
+            let listlength = Number(args[0]);
+            
             Object.keys(econ).forEach(i => {
                 if (client.users.cache.get(i)) {
                     econdata.push(econ[i]);
                 }
             });
+            if(!listlength || isNaN(listlength) || listlength > econdata.length || listlength > 500) listlength = 10;
             let compare = function(a, b) {
                 if (a.money < b.money)
                     return 1;
@@ -26,10 +29,14 @@ module.exports = {
             econdata.sort(compare);
             let msg = '';
             let i = 0;
-            while (i < 10 && econdata[i]) {
+            while (i < listlength && econdata[i]) {
                 let place = i + 1;
                 msg += '`(' + place + ') ' + client.users.cache.get(econdata[i].userid).tag + ': ' + econdata[i].money + Config.currencyName\n';
                 i++;
+            }
+            if(listlength > 10) {
+            message.channel.send('Any amount higher than 10 is sent in DMS, check yours.');
+            return message.author.send(msg)
             }
             message.channel.send(msg);
 
