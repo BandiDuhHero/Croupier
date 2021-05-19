7318bd91ae3e74cdd17b786db8b3f5b41a959799
BandiDuhHero <elibandimail@gmail.com>
2021-05-19 02:47:12 -0500

    Create stocks.js

---
+++ Economy/stocks.js
@@ -0,0 +1,10 @@
+exports.tickers = {
+AT: 'Adryen\'s Traphouse',
+SC: 'Six\'s Commissions',
+BS: 'Bandi\'s Studio',
+AH: 'Ara\'s Halal',
+NW: 'Nacho\'s Whorehouse',
+TM: 'Thylan\'s Manufacturing',
+KS: 'KingBillu\'s Suites',
+SD: 'Sleaze\'s Dealership'
+};
