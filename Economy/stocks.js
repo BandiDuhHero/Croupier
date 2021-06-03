9b6acf44f943ffa960dc3ca8733e94c95c7c5ffe
BandiDuhHero <elibandimail@gmail.com>
2021-06-03 16:39:44 -0500

    Update stocks.js

--- Economy/stocks.js
+++ Economy/stocks.js
@@ -5,6 +5,45 @@ class Stock {
         this.name = name;
         this.price = price;
     }
+    rpc(price) { //random price change
+    let pct = 0;
+    let chance1 = Math.floor(Math.random() * 100);
+    let chance2 = Math.floor(Math.random() * 100);
+    let chance3 = Math.floor(Math.random() * 100);
+    if (chance1 > 75) {
+        if (chance2 > 50) {
+            if (chance3 > 25) {
+                pct = 12;
+            } else pct = 8;
+        } else pct = 4;
+    }
+    if (chance1 < 25) {
+        if (chance2 < 25) {
+            if (chance3 > 25) {
+                pct = 50;
+            } else pct = 25 + Math.floor(Math.random() * 10);
+        } else pct = 10;
+    }
+
+    let updown = Math.floor(Math.random() * 100);
+    if (chance1 <= 10) {
+        updown = 51;
+        pct = 100;
+        if (chance1 > 5) {
+            pct = 500;
+        }
+        if (chance1 === 1) {
+            pct = 1000;
+        }
+    }
+
+    if (updown > 50) {
+        price += Math.floor((pct / 100) * price);
+        return price;
+    }
+    price -= Math.floor((pct / 100) * price);
+    return price;
+}
 };
 
 function rpc(price) { //random price change
